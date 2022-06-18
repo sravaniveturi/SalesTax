e85552fc611409fdde65a444ac06bde1af07c8e3
vsravani <sravani.krishnan@gmail.com>
2022-06-18 10:50:13 +0530

    Initial commit

---
+++ src/main/java/com/tw/salestax/Item.java
@@ -0,0 +1,14 @@
+package com.tw.salestax;
+
+public class Item {
+
+    private final int quantity;
+    private final String name;
+    private final double amount;
+
+    public Item(int quantity, String name, double amount) {
+        this.quantity = quantity;
+        this.name = name;
+        this.amount = amount;
+    }
+}
---
+++ src/test/java/com/tw/salestax/ItemTest.java
@@ -0,0 +1,12 @@
+package com.tw.salestax;
+
+import org.junit.jupiter.api.Test;
+
+public class ItemTest {
+
+    @Test
+    void calculateSalestax() {
+        Item item1 = new Item(1, "book", 12);
+
+    }
+}
