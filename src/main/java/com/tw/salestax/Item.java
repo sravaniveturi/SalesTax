742e979b90aa1ef08476908820b165d1df106663
vsravani <sravani.krishnan@gmail.com>
2022-06-18 14:14:46 +0530

    Add. tests for no sales tax for some items

--- src/main/java/com/tw/salestax/Item.java
+++ src/main/java/com/tw/salestax/Item.java
@@ -1,20 +1,38 @@
 package com.tw.salestax;
 
+import java.util.Arrays;
+import java.util.ResourceBundle;
+
 public class Item {
 
     private final int quantity;
     private final String name;
     private final double amount;
+
     private double salestax;
 
     public Item(int quantity, String name, double amount) {
         this.quantity = quantity;
         this.name = name;
         this.amount = amount;
+        salestax = 0.0;
     }
 
     public double calculateSalesTax() {
+        if(!checkItemExemptFromSalesTax(this))
         this.salestax = this.amount * 0.01;
         return this.salestax;
+    }
+
+    public boolean checkItemExemptFromSalesTax(Item item) {
+        final ResourceBundle bundle = ResourceBundle.getBundle("message");
+        String[] exemptString = bundle.getString("items_exempt").split(",");
+        System.out.println("string: "+ exemptString[0]);
+        for(String exemptItem: exemptString) {
+            if (item.name.contains(exemptItem)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/test/java/com/tw/salestax/ItemTest.java
+++ src/test/java/com/tw/salestax/ItemTest.java
@@ -8,7 +8,14 @@ public class ItemTest {
 
     @Test
     void testSalestaxIsCalculated() {
-        Item item1 = new Item(1, "book", 12);
+        Item item1 = new Item(1, "table", 12);
         assertEquals(0.12, item1.calculateSalesTax());
     }
+
+    @Test
+    void testSalestaxIsZeroForItemsExemptFromTax() {
+        Item item1 = new Item(1, "book", 12);
+        assertEquals(0, item1.calculateSalesTax());
+    }
+
 }
