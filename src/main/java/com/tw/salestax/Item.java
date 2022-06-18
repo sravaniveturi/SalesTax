fb39d915b5f35fb0101a7593ddd99ce07c9301fb
vsravani <sravani.krishnan@gmail.com>
2022-06-18 10:59:00 +0530

    Add. sales tax calculation for all items

--- src/main/java/com/tw/salestax/Item.java
+++ src/main/java/com/tw/salestax/Item.java
@@ -5,10 +5,16 @@ public class Item {
     private final int quantity;
     private final String name;
     private final double amount;
+    private double salestax;
 
     public Item(int quantity, String name, double amount) {
         this.quantity = quantity;
         this.name = name;
         this.amount = amount;
+    }
+
+    public double calculateSalesTax() {
+        this.salestax = this.amount * 0.01;
+        return this.salestax;
     }
 }
--- src/test/java/com/tw/salestax/ItemTest.java
+++ src/test/java/com/tw/salestax/ItemTest.java
@@ -2,11 +2,13 @@ package com.tw.salestax;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ItemTest {
 
     @Test
-    void calculateSalestax() {
+    void testSalestaxIsCalculated() {
         Item item1 = new Item(1, "book", 12);
-
+        assertEquals(0.12, item1.calculateSalesTax());
     }
 }
